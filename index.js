24b729d88d0d3e6f47b3ad8c4a8838171e8c7d77
Ghostachio <mansour.hamza2.0@gmail.com>
2024-01-13 15:02:03 +0100

    practiced templating and conditionnal rendering

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const express= require("express")
+const express= require("express");
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
 
 app.get('/' , function(req, res){
-  res.send('hello this is the home page')
+  res.render('home')
 })
 app.get('/contact' , function(req, res){
-  res.send('hello this is the contact page')
+  res.render('contact')
 })
 app.get('/pricing' , function(req, res){
-  res.send('hello this is the picing page')
+  res.render('pricing')
+})
+app.get('/random' , function(req, res){
+  const rand = Math.floor(Math.random() * 10) + 1;
+  res.render('random', {rand})
 })
 app.get('/r/:subreddit' , function(req, res){
   const {subreddit} = req.params;
-  res.send(`hello you are currently in the ${subreddit} category`)
+  res.render('subreddit', {subreddit});
 });
+
+// app.get('*' , function(req, res){
+//   res.send('oups couldnt find this one')
+// })
 
 
 app.listen(3000, function(){
